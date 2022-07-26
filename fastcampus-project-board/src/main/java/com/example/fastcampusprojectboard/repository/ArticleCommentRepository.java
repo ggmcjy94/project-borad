f7e510a0382a27e1bb726693100441829a96b690
leehyunseok <lhs01071526524@gmail.com>
2022-07-26 22:40:10 +0900

    #12 - repository interface 작성
    db에 접근

---
+++ fastcampus-project-board/src/main/java/com/example/fastcampusprojectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.fastcampusprojectboard.repository;
+
+import com.example.fastcampusprojectboard.domain.Article;
+import com.example.fastcampusprojectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
