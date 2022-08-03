5e0b6e304de2a97ef3811b2c474244fcba9d182b
leehyunseok <lhs01071526524@gmail.com>
2022-08-03 20:34:14 +0900

    #28 - 페이지네이션 기능을 컨트롤러 반영
    태스트는 Page 클래스를 이용하여 테스트함 
    나중에 정렬 기능도 추가 할 예정

--- fastcampus-project-board/src/main/java/com/example/fastcampusprojectboard/controller/ArticleController.java
+++ fastcampus-project-board/src/main/java/com/example/fastcampusprojectboard/controller/ArticleController.java
@@ -5,7 +5,9 @@ import com.example.fastcampusprojectboard.dto.response.ArticleCommentResponse;
 import com.example.fastcampusprojectboard.dto.response.ArticleResponse;
 import com.example.fastcampusprojectboard.dto.response.ArticleWithCommentsResponse;
 import com.example.fastcampusprojectboard.service.ArticleService;
+import com.example.fastcampusprojectboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -24,6 +26,7 @@ import java.util.List;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -32,7 +35,12 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map)
     {
-        map.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
     @GetMapping("/{articleId}")
